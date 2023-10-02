08617d8e535250e2ed0dd622a2924ae0062c9bbe
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-10-02 09:05:39 +0300

    maanantai

---
+++ HTML harjoitukset/pistari.js
@@ -0,0 +1,16 @@
+function jarjesta() {
+    var Luku;
+    Luku = parseInt(document.getElementById('eka').value);
+   
+    document.write("Annoit luvun: " >= 0 );
+
+    if (Luku >= 0 ) {
+        k
+        if (Luku < 0 ) {
+            document.write(" Luku on negatiivinen ");
+        }
+        else (Luku > 0 ) {
+            document.write(" Luku on positiivinen ");
+        }
+    }
+}
--- HTML harjoitukset/tehtava1.js
+++ HTML harjoitukset/tehtava1.js
@@ -1,35 +1,35 @@
 function jarjesta() {
     var numero1, numero2, numero3;
-    numero1 = parseInt(document.getElementById('eka').value);
-    numero2 = parseInt(document.getElementById('toka').value);
-    numero3 = parseInt(document.getElementById('kolmas').value);
-    document.write("Annoit numerot: " + numero1 + ' ' + numero2 + ' ' + numero3 + "<br>");
+    numero1 = parseInt(document.getElementById("eka").value);
+    numero2 = parseInt(document.getElementById("toka").value);
+    numero3 = parseInt(document.getElementById("kolmas").value);
+    document.write("Annoit numerot: " + numero1 + " " + numero2 + " " + numero3 + "<br>");
 
     if (numero1 < numero2 && numero1 < numero3) {
         k
         if (numero2 < numero3) {
-            document.write("Numerojen järjestys: " + numero1 + ' ' + numero2 + ' ' + numero3);
+            document.write("Numerojen järjestys: " + numero1 + " " + numero2 + "" + numero3);
         }
         else {
-            document.write("Numerojen järjestys: " + numero1 + ' ' + numero3 + ' ' + numero2);
+            document.write("Numerojen järjestys: " + numero1 + " " + numero3 + " " + numero2);
         }
     }
 
     else if (numero2 < numero1 && numero2 < numero3) {
         if (numero1 < numero3) {
-            document.write("Numerojen järjestys: " + numero2 + ' ' + numero1 + ' ' + numero3);
+            document.write("Numerojen järjestys: " + numero2 + " " + numero1 + " " + numero3);
         }
         else {
-            document.write("Numerojen järjestys: " + numero2 + ' ' + numero3 + ' ' + numero1);
+            document.write("Numerojen järjestys: " + numero2 + " " + numero3 + " " + numero1);
         }
     }
 
     else if (numero3 < numero1 && numero3 < numero2) {
         if (numero1 < numero2) {
-            document.write("Numerojen järjestys: " + numero3 + ' ' + numero1 + ' ' + numero2);
+            document.write("Numerojen järjestys: " + numero3 + " " + numero1 + " " + numero2);
         }
         else {
-            document.write("Numerojen järjestys: " + numero3 + ' ' + numero2 + ' ' + numero1);
+            document.write("Numerojen järjestys: " + numero3 + " " + numero2 + " " + numero1);
 
         }
     }
@@ -62,12 +62,12 @@ function etsi() {
         suurin = l1;
     }
 
-    document.write("Annoit luvut: " + l1 + ' ' + l2 + ' ' + l3 + ' ' + l4 + ' ' + l5);
+    document.write("Annoit luvut: " + l1 + " " + l2 + " " + l3 + " " + l4 + " " + l5);
     document.write("<br>Suurin niistä on: " + suurin);
 }
 function paritonParillinen() {
     var arvo;
-    arvo = parseInt(document.getElementById('luku').value);
+    arvo = parseInt(document.getElementById("luku").value);
     document.write("Antamasi luku: " + arvo);
 
     if (arvo % 2 == 0) {
@@ -79,7 +79,7 @@ function paritonParillinen() {
     }
 }
 function ajoneuvo() {
-    var age = parseInt(document.getElementById('ika').value);
+    var age = parseInt(document.getElementById("ika").value);
 
     if (age < 16) {
         document.write("Voit ajaa polkupyörää");
@@ -94,17 +94,17 @@ function ajoneuvo() {
     }
 }
 function kaanna() {
-    var lang = document.getElementById('kieli').value;
+    var lang = document.getElementById("kieli").value;
 
-    if (lang == 'eng') {
+    if (lang == "eng") {
         document.write("Hello World!");
     }
 
-    else if (lang == 'swe') {
+    else if (lang == "swe") {
         document.write("Hej Världen!");
     }
 
-    else {
+    else if (lang == "esp"){
         document.write("Hola Mundo!");
     }
 }
