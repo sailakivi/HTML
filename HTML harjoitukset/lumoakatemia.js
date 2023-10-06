2e55ef24dce3490cda2a7133345d2ee62754a3a5
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-10-06 09:06:25 +0300

    lumoakatemia

--- HTML harjoitukset/lumoakatemia.js
+++ HTML harjoitukset/lumoakatemia.js
@@ -63,7 +63,67 @@ metri = 100;
 jaardi = metri / 0.91;
 console.log(jaardi);
 
-let kauppalista[];
+function suurempi(a, b) {
+    if (a > b) {
+        console.log("Suurempi luku on" + a);
+    }
+    else if (b > a) {
+        console.log("Suurempi luku on" + b);
+    }
+    else {
+        console.log("Luvut ovat yhtäsuuret");
+    }
+}
+
+function suurinKolmesta(a, b, c) {
+    if (a > b && a > c) {
+        console.log("Suurin luku on" + a);
+    }
+    else if (b > c) {
+        console.log("Suurin luku on" + b);
+    }
+    else {
+        console.log("Suurin luku on" + c);
+    }
+}
+
+function etuMerkki(luku) {
+    if (luku > 0) {
+        console.log("Luku on negatiivinen");
+    }
+    else if (luku == 0) {
+        console.log("Luku on nolla");
+    }
+    else {
+        console.log("Luku on positiivinen");
+    }
+}
+
+function jaollinen(luku) {
+    if (luku % 5 == 0 && luku % 11 == 0) {
+        console.log("Luku on jaollinen sekä 5 että 11");
+    }
+    else if (luku % 5 == 0 || luku % 11 == 0) {
+        console.log("Luku on jaollinen 5 tai 11");
+    }
+    else {
+        console.log("Luku ei ole jaollinen 5 tai 11");
+    }
+}
+
+function karkausvuosi(vuosi) {
+    if (vuosi % 4 == 0 && vuosi % 100 != 0) {
+        console.log("Vuosi on karkausvuosi");
+    }
+    else if (vuosi % 400 == 0) {
+        console.log("Vuosi on karkausvuosi");
+    }
+    else {
+        console.log("Vuosi ei ole karkausvuosi");
+    }
+}
+
+let kauppalista = [];
 
 kauppalista.push("banaani");
 kauppalista.push("maito");
@@ -81,9 +141,9 @@ const taulukko = [1, 2, 3, 4, 5];
 const pituus = taulukko.lengh;
 console.log(pituus);
 
-const nimet = ["Esko", "Mikko", "Sanna", "Aino", "Marja"];
-nimet.push("Petri");
-console.log(nimet);
+const names = ["Esko", "Mikko", "Sanna", "Aino", "Marja"];
+names.push("Petri");
+console.log(names);
 
 const taulukko1 = [1, 2, 3];
 const taulukko2 = [4, 5, 6];
@@ -94,10 +154,10 @@ const numerot = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const parillisetNumerot = numerot.filter(numero => numero % 2 === 0);
 console.log(parillisetNumerot);
 
-const numerot = [5, 2, 9, 1, 5, 8, 3];
-numerot.sort((a, b) => a - b);
-console.log(numerot);
+const numbers = [5, 2, 9, 1, 5, 8, 3];
+numbers.sort((a, b) => a - b);
+console.log(numbers);
 
-const numerot = [1, 2, 3, 4, 5];
-const kaannettyTaulukko = numerot.reverse();
+const numb = [1, 2, 3, 4, 5];
+const kaannettyTaulukko = numb.reverse();
 console.log(kaannettyTaulukko);
