ba26aa538edbf60c50ddcd70766a9df26e453e87
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-11-07 20:47:53 +0200

    lomake

--- HTML harjoitukset/lomake.js
+++ HTML harjoitukset/lomake.js
@@ -1,22 +1,30 @@
 function laheta() {
-    let enimi = document.getElementById("enimi").value;
-    let snimi = document.getElementById("snimi").value;
-    kouluun = document.quarySelector('input[name="kouluun"]:checked')
-    let lempitunti = document.getElementById("lempitunti").value;
-
-    if (enimi == "") {
+    var etunimi = document.getElementById('enimi').value;
+    var sukunimi = document.getElementById('snimi').value;
+    var koulu = document.getElementsByName('kouluun');
+    var pvalikko = document.getElementById('lempi');
+    var valittu = pvalikko.options[pvalikko.selectedIndex].value;
+    var kouluun = "";
+    for (var i = 0; i < koulu.length; i++)
+    {
+        if (koulu[i].checked) {
+            kouluun = koulu[i].value;
+        }
+    }
+    if (etunimi == "") {
         alert("Anna etunimesi");
     }
-    else if (snimi == "") {
+    else if (sukunimi == "") {
         alert("Anna sukunimesi");
     }
     else if (!kouluun) {
         alert("Kerro millä tulit kouluun");
     }
-    else if (lempitunti == "") {
+    else if (valittu == "") {
         alert("Valitse lempituntisi");
     }
     else {
         alert("Kiitos vastauksestasi");
     }
+
 }
