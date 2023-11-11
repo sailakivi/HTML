2cb0424aaccef2e04c4a20d2e435d39f849344ce
sailakivi <esko.sailakivi@edu.keuda.fi>
2023-11-11 12:32:04 +0200

    etä

--- HTML harjoitukset/random.js
+++ HTML harjoitukset/random.js
@@ -1,41 +1,16 @@
-const url = ' https://randomuser.me/api/ ';
+function random() {
+    let pohja;
+    fetch("https://randomuser.me/api/")
+        .then(response => response.json())
+        .then(data => {
 
-let avatar = document.getElementById('avatar');
-let fullname = document.getElementById('fullname');
-let username = document.getElementById('username');
-let email = document.getElementById('email');
-let city = document.getElementById('city');
-let btn = document.getElementById('btn');
+            pohja = data.results[0];
+            console.log(pohja);
 
-btn.addEventListener("click", function () {
-    fetch(url)
-        .then(handleErrors)
-        .then(parseJSON)
-        .then(updateProfile)
-        .catch(printError)
-});
-
-function handleErrors(res) {
-    if (!res.ok) {
-        throw error(res.status);
-    }
-    console.log(res);
-    return res;
-}
-
-function parseJSON(res) {
-    return res.json();
-}
-
-function updateProfile(profile) {
-    avatar.src = profile.results[0].picture.medium;
-    fullname.innerHTML = profile.results[0].name.first + " " + profile.results[0].name.last;
-    username.innerHTML = profile.results[0].login.username;
-    email.innerHTML = profile.results[0].email;
-    city.innerHTML = profile.results[0].location.city;
-    return 1;
-}
-
-function printError(error) {
-    console.log(error);
+            document.getElementById("random").innerHTML =
+                "<br/> <img src='" + pohja.picture.large + "' id='vasen'>" + "<p>Nimi:" + " " + pohja.name.title + " " + pohja.name.first + " " + pohja.name.last + "</p>" +
+                "<p>Osoite:" + " " + pohja.location.street.name + " " + pohja.location.street.number + " " + pohja.location.city + " " + pohja.location.postcode +
+                +"</p>" + "<br/> <p>Sähköposti: " + " " + pohja.email + "</p>" + " <p>Puhelin: " + " " + pohja.cell + " / " + pohja.phone + "</p>" +
+                "<p> Maa: " + " " + pohja.location.country + "</p>" + "<p>Käyttäjätunnus: " + " " + pohja.login.username + "</p>" + "<p>Salasana: " + " " + pohja.login.password + "</p>"
+        });
 }
